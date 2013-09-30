21410177e02e3ba84d54f567e8f7f725c20b323f
aariste <aariste@gmail.com>
2013-09-30 17:02:26 +0200

    Error encoding

--- AemetAPI.py
+++ AemetAPI.py
@@ -1,6 +1,8 @@
 from xml.etree.ElementTree import parse
+from xml.etree.ElementTree import XMLParser
 import urllib
 import datetime
+import codecs
 
 class Base:
 
@@ -15,19 +17,31 @@ class Base:
 		self.cota_nieve = []
 		self.estado_cielo = []
 		self.viento = []
+		self.racha = []
+		self.temperatura_maxima = 0
+		self.temperatura_minima = 0
+		self.temperatura_horas = []
+		self.sensacion_termica_maxima = 0
+		self.sensacion_termica_minima = 0
+		self.sensacion_termica = []
+		self.humedad_maxima = 0
+		self.humedad_minima = 0
+		self.humedad = []
+		self.uv_max = 0
 
 		self.__load_xml()
 
 	def __load_xml(self):
 		self.rss = parse(urllib.urlopen(self.__url)).getroot()
 
-		self.__load_datos_base()		
+		self.__load_datos_base()
 
 	def __load_datos_base(self):
 		self.__fecha_de_actualizacion = self.rss.find('elaborado').text
-		self.__localidad = self.rss.find('nombre').text
+		self.__localidad = self.rss.find('nombre').text.encode('latin_1')
 		self.__provincia = self.rss.find('provincia').text
 
+	'''Parte publica'''
 	def get_fecha_actualizacion(self):
 		return self.__fecha_de_actualizacion
 
@@ -37,83 +51,107 @@ class Base:
 	def get_provincia(self):
 		return self.__provincia
 
-class Localidad(Base):
-
-	def __init__(self, codigo_postal):
-		url = 'http://www.aemet.es/xml/municipios/localidad_' + codigo_postal + '.xml'
-		Base.__init__(self, url)
-
-	def a(self):
-		a = '2013-09-30'
-
-		return list(self.rss.find("prediccion/dia[@fecha='" + a + "']"))
-
-
-	def __obtener_primer_dia(self):
-		dia = self.rss.find("prediccion/dia")
-		return dia.get('fecha')
-
-	def obtener_precipitacion(self, dias):
-		primer_dia = self.__obtener_primer_dia()
-
-		fecha = Helper.anadir_dias(self, primer_dia, dias)
-
-		nodo = "prediccion/dia[@fecha='" + fecha + "']/prob_precipitacion"
-
-		for element in self.rss.findall(nodo):
-			self.precipitacion.append([element.get('periodo'), element.text])
-
+	def get_precipitacion(self):
 		return self.precipitacion
 
-	def obtener_cota_nieve(self, dias):
-		primer_dia = self.__obtener_primer_dia()
-
-		fecha = Helper.anadir_dias(self, primer_dia, dias)
-
-		nodo = "prediccion/dia[@fecha='" + fecha + "']/cota_nieve_prov"
-
-		for element in self.rss.findall(nodo):
-			self.cota_nieve.append([element.get('periodo'), element.text])
-
+	def get_cota_nieve(self):
 		return self.cota_nieve
 
-	def obtener_estado_cielo(self, dias):
-		primer_dia = self.__obtener_primer_dia()
-
-		fecha = Helper.anadir_dias(self, primer_dia, dias)
-
-		nodo = "prediccion/dia[@fecha='" + fecha + "']/estado_cielo"
-
-		for element in self.rss.findall(nodo):
-			self.estado_cielo.append([element.get('periodo'), element.get('descripcion'), element.text])
-
+	def get_estado_cielo(self):
 		return self.estado_cielo
 
-	def obtener_viento(self, dias):
-		direccion = ''
-		velocidad = ''
-
-		primer_dia = self.__obtener_primer_dia()
-
-		fecha = Helper.anadir_dias(self, primer_dia, dias)
-
-		nodo = "prediccion/dia[@fecha='" + fecha + "']/viento"
-
-		for element in self.rss.findall(nodo):
-			periodo = element.get('periodo')
-			
-			nodo_direccion = nodo + '/direccion'
-			nodo_velocidad = nodo + '/velocidad'
-
-			for el_direccion in element.findall(nodo_direccion):
-				direccion = el_direccion.text
-
-			for el_velocidad in element.findall(nodo_velocidad):
-				velocidad = el_velocidad.text
-
-			self.viento.append([periodo, direccion, velocidad])
-
+	def get_viento(self):
 		return self.viento
+
+	def get_racha(self):
+		return self.racha
+
+	def get_temperatura_maxima(self):
+		return self.temperatura_maxima
+
+	def get_temperatura_minima(self):
+		return self.temperatura_minima
+		
+	def get_temperatura_horas(self):
+		return self.temperatura_horas
+
+	def get_sensacion_termica_maxima(self):
+		return self.sensacion_termica_maxima
+
+	def get_sensacion_termica_minima(self):
+		return self.sensacion_termica_minima
+
+	def get_sensacion_termica(self):
+		return self.sensacion_termica
+
+	def get_humedad_maxima(self):
+		return self.humedad_maxima
+
+	def get_humedad_minima(self):
+		return self.humedad_minima
+
+	def get_humedad(self):
+		return self.humedad
+
+	def get_uv_max(self):
+		return self.uv_max
+
+class Localidad(Base):
+
+	def __init__(self, codigo_postal, fecha):
+		url = 'http://www.aemet.es/xml/municipios/localidad_' + codigo_postal + '.xml'
+		Base.__init__(self, url)		
+		self.__load_datos(fecha)
+
+	def __load_datos(self, fecha):
+		nodo = self.rss.find("prediccion/dia[@fecha='" + fecha + "']")
+
+		'''Probabilidad de precipitacion'''
+		for elem in nodo.findall('prob_precipitacion'):
+			self.precipitacion.append([elem.get('periodo'), elem.text])
+
+		'''Cota de nieve'''
+		for elem in nodo.findall('cota_nieve_prov'):
+			self.cota_nieve.append([elem.get('periodo'), elem.text])
+		
+		'''Estado'''
+		for elem in nodo.findall('estado_cielo'):
+			self.estado_cielo.append([elem.get('periodo'), elem.get('descripcion')])
+
+		'''Viento'''
+		for elem in nodo.findall('viento'):					
+			self.viento.append([elem.get('periodo'), elem.find('direccion').text, elem.find('velocidad').text])
+
+		'''Racha maxima'''
+		for elem in nodo.findall('racha_max'):
+			self.racha.append([elem.get('periodo'), elem.text])
+
+		'''Temperatura'''
+		temperatura_maxima = nodo.find('temperatura/maxima')
+		temperatura_minima = nodo.find('temperatura/minima')
+
+		for elem in nodo.findall('temperatura/dato'):
+			self.temperatura_horas.append([elem.get('hora'), elem.text])
+
+		'''Sensacion termica'''
+		sensacion_termica_maxima = nodo.find('sens_termica/maxima')
+		sensacion_termica_minima = nodo.find('sens_termica/minima')
+
+		for elem in nodo.findall('sens_termica/dato'):
+			self.sensacion_termica.append([elem.get('hora'), elem.text])
+
+		'''Humedad'''
+		humedad_maxima = nodo.find('humedad_relativa/maxima')
+		humedad_minima = nodo.find('humedad_relativa/minima')
+
+		for elem in nodo.findall('humedad_relativa/dato'):
+			self.humedad.append([elem.get('hora'), elem.text])
+
+		self.uv_max = nodo.find('uv_max').text		
+	
+	def __obtener_primer_dia(self):
+		dia = self.rss.find("prediccion/dia")
+		return dia.get('fecha')	
 
 class Helper:
 	def __init__(self):
