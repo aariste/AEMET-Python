73354af8fadd56bdb80c7af7b26455c58885fa9a
Adrià Ariste <adria@ariste.info>
2014-04-02 16:42:05 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+"""
+Setup script.
+"""
+
+from distutils.core import setup
+
+
+setup(name = "AEMET",
+    version = "1.0.0",
+    description = "Consultas AEMET",
+    long_description = "Libreria en python para consultar los datos de la AEMET",
+    author = "Adria Ariste",
+    author_email = 'adria@ariste.info',
+    url = "https://github.com/aariste/AEMET-Python",
+    download_url = "https://github.com/aariste/AEMET-Python/archive/master.zip",
+    platforms = ['any'],
+
+    license = "",
+
+    package_dir = {'AEMET': 'src/'},
+    packages = ['AEMET'],
+)
+
